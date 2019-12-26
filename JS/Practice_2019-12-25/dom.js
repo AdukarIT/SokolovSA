8c556a9001b3c21ef92c743920d29714fcf3383a
Semyon-Sokolov <5598232@mail.ru>
2019-12-27 01:21:51 +0300

    modified files

--- JS/Practice_2019-12-25/dom.js
+++ JS/Practice_2019-12-25/dom.js
@@ -1,3 +1,13 @@
+/*1. Все элементы label внутри таблицы. Должно быть 3 элемента.*/
 let label = document.getElementsByTagName("label");
 	console.log(label);
+
+// for (let label of document.body.children) {
+//   console.log(label); 
+// }
+
+/*2. Первую ячейку таблицы (со словом "Возраст").*/
+let age = document.table.firstElementChild;
+	console.log(age);
+
 	
